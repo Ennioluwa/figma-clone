4a4395870a30395ff89825f63953fa8b51870a3c
Enioluwa <enioluwaarowo@gmail.com>
2024-05-08 14:13:42 +0100

    add liveblocks

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,13 +1,14 @@
-import { Inter as FontSans } from "next/font/google";
+import { Work_Sans as FontSans } from "next/font/google";
 import { Poppins } from "next/font/google";
 
-export const fontSans = FontSans({
+export const workSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const fontPoppins = Poppins({
-weight: ["400", "500", "600", "700", "800", "900"],
-subsets: ["latin"],
-variable: "--font-poppins",
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
 });
